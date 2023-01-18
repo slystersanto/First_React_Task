e0dbda95ea01d59403cc13c0ef282dc99d4e5825
slystersanto <santhosh.mech.19@gmail.com>
2023-01-19 00:39:36 +0530

    commit react

--- src/App.js
+++ src/App.js
@@ -2,24 +2,68 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const getdata=[
+    {
+      title:"FREE",
+      sub_title:"$0/month",
+      notvalid:"❌",
+      valid:"✔️",
+    },
+    {
+      title:"PLUS",
+      sub_title:"$9/month",
+      notvalid:"❌",
+      valid:"✔️",
+    },
+    {
+      title:"PRO",
+      sub_title:"$49/month",
+      notvalid:"❌",
+      valid:"✔️",
+    },
+
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {getdata.map((item,index)=>(
+        <Subscription
+        key={index}
+        title={item.title}
+        sub_title={item.sub_title}
+        notvalid={item.notvalid}
+        valid={item.valid}
+        />
+        ))}
     </div>
   );
 }
 
 export default App;
+
+
+function Subscription(props){
+  return(
+   <div className='container'>
+    <div className='cards'>
+      <p className='title'>{props.title}</p>
+        <p className='sub_title'>{props.sub_title}</p>
+          <hr></hr>
+
+          <ul>
+            <li>{props.valid}Single User</li>
+            <li>{props.valid}5GB Storage</li>
+            <li>{props.valid}Unlimited Public Projects</li>
+            <li>{props.valid}Community Access</li>
+            <li>{props.notvalid}Unlimited Private Projects</li>
+            <li>{props.notvalid}Dedicated Phone Support</li>
+            <li>{props.notvalid}Free Subdomain</li>
+            <li>{props.notvalid}Monthly Status Reports</li>
+          </ul>
+        <div>  <button className='btn-bottom' type="button"><b>BUTTON</b></button>
+
+        </div>
+      
+    </div>
+   </div>
+  )
+}

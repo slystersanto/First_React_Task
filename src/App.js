ed51c12ad97b5a096fb42e5af9516fd8d09daeae
slystersanto <santhosh.mech.19@gmail.com>
2023-04-14 23:48:17 +0530

    final-commit

--- src/App.js
+++ src/App.js
@@ -1,69 +1,164 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  const getdata=[
-    {
-      title:"FREE",
-      sub_title:"$0/month",
-      notvalid:"❌",
-      valid:"✔️",
-    },
-    {
-      title:"PLUS",
-      sub_title:"$9/month",
-      notvalid:"❌",
-      valid:"✔️",
-    },
-    {
-      title:"PRO",
-      sub_title:"$49/month",
-      notvalid:"❌",
-      valid:"✔️",
-    },
 
+
+function App() {
+  const data = [{
+    plan: "free",
+    price: "$0",
+    features: [
+      {
+        title: "Single User",
+        correct: true
+      },
+      {
+        title: "5GB Storage",
+        correct: true
+      },
+      {
+        title: "Unlimited Public Projects",
+        correct: true
+      },
+      {
+        title: "Community Access",
+        correct: true
+      },
+      {
+        title: "Unlimited Private Projects",
+        correct: false
+      },
+      {
+        title: "Dedicated Phone Support",
+        correct: false
+      },
+      {
+        title: "Free Subdomain",
+        correct: false
+      },
+      {
+        title: "Monthly Status Reports",
+        correct: false
+      }
+    ]
+  },
+  {
+    plan: "PLUS",
+    price: "$9",
+    features: [
+      {
+        title: "5 Users",
+        correct: true,
+        highlight: true
+      },
+      {
+        title: "50GB Storage",
+        correct: true
+      },
+      {
+        title: "Unlimited Public Projects",
+        correct: true
+      },
+      {
+        title: "Community Access",
+        correct: true
+      },
+      {
+        title: "Unlimited Private Projects",
+        correct: true
+      },
+      {
+        title: "Dedicated Phone Support",
+        correct: true
+      },
+      {
+        title: "Free Subdomain",
+        correct: true
+      },
+      {
+        title: "Monthly Status Reports",
+        correct: false
+      }
+    ]
+  },
+  {
+    plan: "PRO",
+    price: "$49",
+    features: [
+      {
+        title: "Unlimited Users",
+        correct: true,
+        highlight: true
+      },
+      {
+        title: "150GB Storage",
+        correct: true
+      },
+      {
+        title: "Unlimited Public Projects",
+        correct: true
+      },
+      {
+        title: "Community Access",
+        correct: true
+      },
+      {
+        title: "Unlimited Private Projects",
+        correct: true
+      },
+      {
+        title: "Dedicated Phone Support",
+        correct: true
+      },
+      {
+        title: "Unlimited Free Subdomains",
+        correct: true
+      },
+      {
+        title: "Monthly Status Reports",
+        correct: true
+      }
+    ]
+  }
   ];
+
   return (
-    <div className='App'>
-      {getdata.map((item,index)=>(
-        <Subscription
-        key={index}
-        title={item.title}
-        sub_title={item.sub_title}
-        notvalid={item.notvalid}
-        valid={item.valid}
-        />
-        ))}
+    <div className="App">
+      <section class="pricing py-5">
+        <div class="container">
+          <div class="row">
+            {data.map((ele) => {
+              return <Card card={ele} />
+            })}
+
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
+function Card({ card }) {
+  return (
+    <div class="col-lg-4">
+      <div class="card mb-5 mb-lg-0">
+        <div class="card-body">
+          <h5 class="card-title text-muted text-uppercase text-center">{card.plan}</h5>
+          <h6 class="card-price text-center">{card.price}<span class="period">/month</span></h6>
+          <hr />
+          <ul class="fa-ul">
+            {card.features.map((item) => {
 
-export default App;
-
-
-function Subscription(props){
-  return(
-   <div className='container'>
-    <div className='cards'>
-      <p className='title'>{props.title}</p>
-        <p className='sub_title'>{props.sub_title}</p>
-          <hr></hr>
-
-          <ul>
-            <li>{props.valid}Single User</li>
-            <li>{props.valid}5GB Storage</li>
-            <li>{props.valid}Unlimited Public Projects</li>
-            <li>{props.valid}Community Access</li>
-            <li>{props.notvalid}Unlimited Private Projects</li>
-            <li>{props.notvalid}Dedicated Phone Support</li>
-            <li>{props.notvalid}Free Subdomain</li>
-            <li>{props.notvalid}Monthly Status Reports</li>
+              return <li class={item.correct ? "" : "text-muted"}><span class="fa-li"><i class={item.correct ? "fas fa-check" : "fas fa-times"}></i></span>{item.highlight ? <strong>{item.title}</strong> : <span>{item.title}</span>}</li>
+            }
+            )}
           </ul>
-        <div>  <button className='btn-bottom' type="button"><b>BUTTON</b></button>
-
+          <div class="d-grid">
+            <a href="#" class="btn btn-primary text-uppercase">Button</a>
+          </div>
         </div>
-      
+      </div>
     </div>
-   </div>
   )
 }
+
+export default App;
